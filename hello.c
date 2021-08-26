5c375a17f8202a0dfad048023b4377795458ca55
Brandon Clear <bclear2@huskers.unl.edu>
2021-08-26 19:23:30 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Brandon Clear
+ * Date: 2021/08/26
+ */
+ 
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Brandon Clear\n");
+
+    return 0;
+
+}
